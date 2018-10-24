9d299c0811b31ac970614a049238f19200de348b
Kusachev Sergey <s7095509@yandex.ru>
2018-10-24 17:22:06 +0300

    add askName

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/local/bin/node
+import askName from '..';
+
 console.log('Welcome to the Brain Games!');
+console.log(askName());
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readline from 'readline-sync';
+
+export default () => {
+    const name = readline.question('\nMay I have your name? ');
+    return `Hello, ${name}!`;
+};
+
