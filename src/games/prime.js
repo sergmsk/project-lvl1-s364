a19c9ef54e8aa45adbdc3611c37adf390ac2b8ee
Kusachev Sergey <s7095509@yandex.ru>
2018-10-29 01:00:45 +0300

    fix isPrime

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,7 +8,7 @@ const specification = 'Answer "yes" if given number is prime. Otherwise answer "
 
 const isPrime = (number) => {
   if (number < 2) return false;
-  for (let i = 2; i < number; i += 1) {
+  for (let i = 2; i < Math.ceil(Math.sqrt(number)); i += 1) {
     if (number % i === 0) return false;
   }
   return true;
