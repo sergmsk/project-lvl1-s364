86784916f4dae13c4d9f1ed2d0df48f6e005245a
Kusachev Sergey <s7095509@yandex.ru>
2018-10-28 17:10:19 +0300

    primeGenerator -> utils

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,36 +1,12 @@
 import game from '..';
-import { getRandomInt } from '../utils';
+import { getRandomInt, primeGenerator } from '../utils';
 
 const minRandomNum = 2;
-const maxRandomNum = 20;
+const maxRandomNum = 100;
 
 const specification = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const setPrime = new Set();
-const primeGenerator = (max) => {
-  const arrMatrix = [];
-  for (let i = 2; i <= max; i += 1) {
-    arrMatrix[i] = true;
-  }
-  let p = 2;
-  do {
-    for (let j = 2 * p; j <= max; j += p) {
-      arrMatrix[j] = false;
-    }
-    for (let k = p + 1; k <= max; k += 1) {
-      if (arrMatrix[k]) {
-        p = k;
-        break;
-      }
-    }
-  } while (p * p <= max);
-
-  arrMatrix.forEach((el, index) => {
-    if (el) setPrime.add(index);
-  });
-  return setPrime;
-};
-primeGenerator(maxRandomNum);
+const setPrime = primeGenerator(maxRandomNum);
 const isPrime = n => setPrime.has(n);
 
 const generator = () => {
--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,25 @@
-// eslint-disable-next-line
 export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const primeGenerator = (max) => {
+  const setPrime = new Set();
+  const arrMatrix = [];
+  for (let i = 2; i <= max; i += 1) {
+    arrMatrix[i] = true;
+  }
+  let p = 2;
+  do {
+    for (let j = 2 * p; j <= max; j += p) {
+      arrMatrix[j] = false;
+    }
+    for (let k = p + 1; k <= max; k += 1) {
+      if (arrMatrix[k]) {
+        p = k;
+        break;
+      }
+    }
+  } while (p * p <= max);
+
+  arrMatrix.forEach((el, index) => {
+    if (el) setPrime.add(index);
+  });
+  return setPrime;
+};
